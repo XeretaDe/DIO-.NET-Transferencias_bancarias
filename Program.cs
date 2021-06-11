3a07418276be886cefe8b3d3d7942a91fea67314
XeretaDe <rodrigomcariello@gmail.com>
2021-06-11 03:29:06 -0300

    ADD_Arquivos_Base
    Add de arquivos base com o comando dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_.NET_Transferencias_bancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
